dcebccf35ec14d75f9804316a365d5d41bee8732
Samantha Foong <shenli574@gmail.com>
2023-05-23 18:51:24 +0100

    date specified

--- get_latest_weatherfc.py
+++ get_latest_weatherfc.py
@@ -5,6 +5,7 @@ import xarray as xr
 client = Client()
 parameters = ['10u', '10v', 'sp', 'msl']
 client.retrieve(
+    date=0,
     stream="oper",
     type="fc",
     step=[ 0,  3,  6,  9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63, 66, 69, 72, 75, 78, 81, 84, 87, 90],
@@ -14,6 +15,7 @@ client.retrieve(
 )
 parameters = ['2t']
 client.retrieve(
+    date=0,
     stream="oper",
     type="fc",
     step=[ 0,  3,  6,  9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63, 66, 69, 72, 75, 78, 81, 84, 87, 90],
