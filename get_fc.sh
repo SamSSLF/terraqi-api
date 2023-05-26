e6c4dac1eab7aa048898cc518a4a84f0549cd45a
Samantha Foong <shenli574@gmail.com>
2023-05-26 19:12:20 +0100

    deleted unnecessary scripts

--- get_fc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/terraqi-backend-v2
-conda activate base
-/home/ubuntu/terraqi-backend-v2/venv/bin/python /home/ubuntu/terraqi-backend-v2/get_weatherfc.py
-/home/ubuntu/terraqi-backend-v2/venv/bin/python /home/ubuntu/terraqi-backend-v2/convert_to_nc.py
