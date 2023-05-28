ca4def76a3b1012c24600464ed956e28467f17ef
Samantha Foong <shenli574@gmail.com>
2023-05-28 15:28:26 +0100

    daily wind endpoint

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request, abort
-from functions import interp_inputs, get_windfc, is_outside_uk
+from functions import interp_inputs, get_windfc, is_outside_uk, get_daily_windfc
 
 app = Flask(__name__)
 
@@ -18,7 +18,22 @@ def get_fc():
         abort(400, "Queried coordinates are beyond the geographical scope of this dataset.")
 
     df = interp_inputs(latitude, longitude)
-    result = get_windfc(df)
+    result = get_windfc(df).to_json()
+
+    return result
+
+
+@app.route('/windfc/daily', methods=['POST'])
+def get_daily_fc():
+    latitude = request.json['lat']
+    longitude = request.json['lon']
+
+    if is_outside_uk(latitude, longitude):
+        abort(400, "Queried coordinates are beyond the geographical scope of this dataset.")
+
+    df = interp_inputs(latitude, longitude)
+    df = get_windfc(df)
+    result = get_daily_windfc(df).to_json(orient='index')
 
     return result
 
